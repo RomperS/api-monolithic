e4bb5560ecf1c448d8a8a03d5cc1a36547326d94
Oswaldo Lopez <77045040+RomperS@users.noreply.github.com>
2025-11-01 23:12:06 +0000

    feat: initial commit

---
+++ src/main/java/com/olo/apimonolithic/ApiMonolithicApplication.java
@@ -0,0 +1,13 @@
+package com.olo.apimonolithic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMonolithicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiMonolithicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/olo/apimonolithic/ApiMonolithicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olo.apimonolithic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMonolithicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
