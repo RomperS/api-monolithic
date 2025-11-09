88356a3f1d2813772789f7e30c76bb613c534b6c
Oswaldo Lopez <77045040+RomperS@users.noreply.github.com>
2025-11-09 20:08:57 +0000

    feat(common): add DomainException for domain-level errors

---
+++ src/main/java/com/olo/apimonolithic/common/domain/exception/DomainException.java
@@ -0,0 +1,11 @@
+package com.olo.apimonolithic.common.domain.exception;
+
+public class DomainException extends RuntimeException {
+    public DomainException(String message) {
+        super(message);
+    }
+
+    public DomainException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
