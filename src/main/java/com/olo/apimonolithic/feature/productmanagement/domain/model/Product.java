07d1c214847a26c0a7061a8b6108b6540cedbcbb
Oswaldo Lopez <77045040+RomperS@users.noreply.github.com>
2025-11-10 17:32:11 +0000

    refactor(product): change value type to BigDecimal

--- src/main/java/com/olo/apimonolithic/feature/productmanagement/domain/model/Product.java
+++ src/main/java/com/olo/apimonolithic/feature/productmanagement/domain/model/Product.java
@@ -2,10 +2,12 @@ package com.olo.apimonolithic.feature.productmanagement.domain.model;
 
 import com.olo.apimonolithic.feature.categorymanagement.domain.model.Category;
 
+import java.math.BigDecimal;
+
 public record Product(
         Long id,
         String name,
         Category category,
-        Long value
+        BigDecimal value
 ) {
 }
