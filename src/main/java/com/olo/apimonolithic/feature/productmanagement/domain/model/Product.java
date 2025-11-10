530d3ae9457b0f5c82b0a9eeee713a9375069b68
Oswaldo Lopez <77045040+RomperS@users.noreply.github.com>
2025-11-10 22:32:40 +0000

    refactor(product): change Category field to List<Category>

--- src/main/java/com/olo/apimonolithic/feature/productmanagement/domain/model/Product.java
+++ src/main/java/com/olo/apimonolithic/feature/productmanagement/domain/model/Product.java
@@ -3,11 +3,12 @@ package com.olo.apimonolithic.feature.productmanagement.domain.model;
 import com.olo.apimonolithic.feature.categorymanagement.domain.model.Category;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 public record Product(
         Long id,
         String name,
-        Category category,
+        List<Category> categories,
         BigDecimal value
 ) {
 }
