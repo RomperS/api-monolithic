c4d407e4dace2202951ed8a378467e09a6fc37a9
Oswaldo Lopez <77045040+RomperS@users.noreply.github.com>
2025-11-11 17:57:21 +0000

    refactor(product): change categories from List to Set

--- src/main/java/com/olo/apimonolithic/feature/productmanagement/domain/model/Product.java
+++ src/main/java/com/olo/apimonolithic/feature/productmanagement/domain/model/Product.java
@@ -3,12 +3,12 @@ package com.olo.apimonolithic.feature.productmanagement.domain.model;
 import com.olo.apimonolithic.feature.categorymanagement.domain.model.Category;
 
 import java.math.BigDecimal;
-import java.util.List;
+import java.util.Set;
 
 public record Product(
         Long id,
         String name,
-        List<Category> categories,
+        Set<Category> categories,
         BigDecimal value
 ) {
 }
