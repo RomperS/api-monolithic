0d1f7b479cea17958099c8442e6bb8b34811b1d9
Oswaldo Lopez <77045040+RomperS@users.noreply.github.com>
2025-11-11 01:31:50 +0000

    chore(config): register CategoryExistsImpl in CategoryService wiring

--- src/main/java/com/olo/apimonolithic/common/infrastructure/config/ApplicationConfig.java
+++ src/main/java/com/olo/apimonolithic/common/infrastructure/config/ApplicationConfig.java
@@ -1,10 +1,7 @@
 package com.olo.apimonolithic.common.infrastructure.config;
 
 import com.olo.apimonolithic.feature.categorymanagement.application.CategoryService;
-import com.olo.apimonolithic.feature.categorymanagement.application.impl.CreateCategoryImpl;
-import com.olo.apimonolithic.feature.categorymanagement.application.impl.DeleteCategoryImpl;
-import com.olo.apimonolithic.feature.categorymanagement.application.impl.FindCategoryImpl;
-import com.olo.apimonolithic.feature.categorymanagement.application.impl.RenameCategoryImpl;
+import com.olo.apimonolithic.feature.categorymanagement.application.impl.*;
 import com.olo.apimonolithic.feature.categorymanagement.domain.ports.outbounds.CategoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -20,7 +17,8 @@ public class ApplicationConfig {
                 new CreateCategoryImpl(categoryRepository),
                 new DeleteCategoryImpl(categoryRepository),
                 new FindCategoryImpl(categoryRepository),
-                new RenameCategoryImpl(categoryRepository)
+                new RenameCategoryImpl(categoryRepository),
+                new CategoryExistsImpl(categoryRepository)
         );
     }
 }
