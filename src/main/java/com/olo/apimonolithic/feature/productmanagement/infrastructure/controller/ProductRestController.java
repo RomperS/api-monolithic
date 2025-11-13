e14d6e4b599d051397a1c23a8de7faefbb654e68
Oswaldo Lopez <77045040+RomperS@users.noreply.github.com>
2025-11-13 17:22:33 +0000

    feat(product): add ProductRestController

---
+++ src/main/java/com/olo/apimonolithic/feature/productmanagement/infrastructure/controller/ProductRestController.java
@@ -0,0 +1,59 @@
+package com.olo.apimonolithic.feature.productmanagement.infrastructure.controller;
+
+import com.olo.apimonolithic.feature.productmanagement.application.ProductService;
+import com.olo.apimonolithic.feature.productmanagement.infrastructure.dto.ProductRequestDto;
+import com.olo.apimonolithic.feature.productmanagement.infrastructure.dto.ProductResponseDto;
+import com.olo.apimonolithic.feature.productmanagement.infrastructure.dto.ProductSearchRequestDTO;
+import com.olo.apimonolithic.feature.productmanagement.infrastructure.dto.ProductUpdateRequestDTO;
+import com.olo.apimonolithic.feature.productmanagement.infrastructure.mapper.ProductMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/products")
+public class ProductRestController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    public ResponseEntity<ProductResponseDto> create(@RequestBody ProductRequestDto dto){
+        return ResponseEntity.ok().body(ProductMapper.modelToDto(productService.create(ProductMapper.dtoToCreateCommand(dto))));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductResponseDto>> findAll() {
+        return ResponseEntity.ok().body(productService.findAll().stream().map(ProductMapper::modelToDto).toList());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponseDto> findById(@PathVariable Long id) {
+        return ResponseEntity.ok().body(ProductMapper.modelToDto(productService.findById(id)));
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<ProductResponseDto>> findByCriteria(@RequestBody @Validated ProductSearchRequestDTO dto) {
+        return ResponseEntity.ok().body(
+                productService.findByFilter(
+                        ProductMapper.dtoToSearchCommand(dto))
+                        .stream()
+                        .map(ProductMapper::modelToDto)
+                        .toList()
+        );
+    }
+
+    @PutMapping
+    public ResponseEntity<ProductResponseDto> update(@RequestBody ProductUpdateRequestDTO dto) {
+        return ResponseEntity.ok().body(ProductMapper.modelToDto(productService.update(ProductMapper.dtoToUpdateCommand(dto))));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        productService.delete(id);
+        return ResponseEntity.ok().build();
+    }
+}
