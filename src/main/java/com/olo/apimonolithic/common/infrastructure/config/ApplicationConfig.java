1cc11a73caa79257f84fb6fa2e782964be9ed9f6
Oswaldo Lopez <77045040+RomperS@users.noreply.github.com>
2025-11-13 17:22:14 +0000

    feat(product): register ProductService facade bean in ApplicationConfig

--- src/main/java/com/olo/apimonolithic/common/infrastructure/config/ApplicationConfig.java
+++ src/main/java/com/olo/apimonolithic/common/infrastructure/config/ApplicationConfig.java
@@ -2,7 +2,15 @@ package com.olo.apimonolithic.common.infrastructure.config;
 
 import com.olo.apimonolithic.feature.categorymanagement.application.CategoryService;
 import com.olo.apimonolithic.feature.categorymanagement.application.impl.*;
+import com.olo.apimonolithic.feature.categorymanagement.domain.ports.inbound.CategoryExists;
+import com.olo.apimonolithic.feature.categorymanagement.domain.ports.inbound.FindCategory;
 import com.olo.apimonolithic.feature.categorymanagement.domain.ports.outbounds.CategoryRepository;
+import com.olo.apimonolithic.feature.productmanagement.application.ProductService;
+import com.olo.apimonolithic.feature.productmanagement.application.impl.CreateProductImpl;
+import com.olo.apimonolithic.feature.productmanagement.application.impl.DeleteProductImpl;
+import com.olo.apimonolithic.feature.productmanagement.application.impl.FindProductImpl;
+import com.olo.apimonolithic.feature.productmanagement.application.impl.UpdateProductImpl;
+import com.olo.apimonolithic.feature.productmanagement.domain.port.outbound.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,6 +27,16 @@ public class ApplicationConfig {
                 new FindCategoryImpl(categoryRepository),
                 new RenameCategoryImpl(categoryRepository),
                 new CategoryExistsImpl(categoryRepository)
+        );
+    }
+
+    @Bean
+    public ProductService productService(ProductRepository productRepository, CategoryExists categoryExists, FindCategory findCategory) {
+        return new ProductService(
+                new CreateProductImpl(productRepository, categoryExists, findCategory),
+                new DeleteProductImpl(productRepository),
+                new FindProductImpl(productRepository, categoryExists),
+                new UpdateProductImpl(productRepository, findCategory)
         );
     }
 }
