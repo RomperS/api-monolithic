a1aca435b2260a56f3addf2e5be61caf234028e4
Oswaldo Lopez <77045040+RomperS@users.noreply.github.com>
2025-11-13 18:59:36 +0000

    refactor(validation): enhance ControllerAdvice to provide detailed field-specific messages for all Product DTOs

--- src/main/java/com/olo/apimonolithic/common/infrastructure/controller/ControllerAdvice.java
+++ src/main/java/com/olo/apimonolithic/common/infrastructure/controller/ControllerAdvice.java
@@ -8,15 +8,19 @@ import com.olo.apimonolithic.common.domain.exception.product.ProductNotFoundExce
 import com.olo.apimonolithic.common.infrastructure.utils.ResponseExceptionUtil;
 import com.olo.apimonolithic.feature.categorymanagement.infrastructure.dto.CategoryRequestDto;
 import com.olo.apimonolithic.feature.productmanagement.infrastructure.dto.ProductRequestDto;
+import com.olo.apimonolithic.feature.productmanagement.infrastructure.dto.ProductSearchRequestDTO;
+import com.olo.apimonolithic.feature.productmanagement.infrastructure.dto.ProductUpdateRequestDTO;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.io.IOException;
+import java.util.List;
 
 @RestControllerAdvice
 @RequiredArgsConstructor
@@ -40,19 +44,56 @@ public class ControllerAdvice {
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public void handleValidation(MethodArgumentNotValidException ex, HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String errorMessage;
+    public void handleValidation(MethodArgumentNotValidException ex,
+                                 HttpServletRequest request,
+                                 HttpServletResponse response) throws IOException {
+
         HttpStatus status = HttpStatus.BAD_REQUEST;
         Object target = ex.getBindingResult().getTarget();
+        String errorMessage = "Invalid request: Validation failed.";
 
         if (target instanceof CategoryRequestDto) {
             errorMessage = "Invalid category data: Missing category name.";
         } else if (target instanceof ProductRequestDto) {
-            errorMessage = "Invalid product data: Missing target category.";
-        } else {
-            errorMessage = "Invalid request: Validation failed.";
+            List<FieldError> errors = ex.getBindingResult().getFieldErrors();
+            if (!errors.isEmpty()) {
+                FieldError fieldError = errors.get(0);
+                String field = fieldError.getField();
+                switch (field) {
+                    case "name" -> errorMessage = "Invalid product data: Missing name.";
+                    case "value" -> errorMessage = "Invalid product data: Missing or invalid value.";
+                    case "categoriesId" -> errorMessage = "Invalid product data: Must include at least one category.";
+                }
+            }
+        } else if (target instanceof ProductSearchRequestDTO) {
+            List<FieldError> errors = ex.getBindingResult().getFieldErrors();
+            if (!errors.isEmpty()) {
+                FieldError fieldError = errors.get(0);
+                String field = fieldError.getField();
+                switch (field) {
+                    case "categoryId" -> errorMessage = "Invalid product data: Missing category id.";
+                    case "page" -> errorMessage = "Invalid product data: Missing or invalid page number.";
+                    case "pageSize" -> errorMessage = "Invalid product data: Missing or invalid page size number.";
+                }
+            }
+        } else if (target instanceof ProductUpdateRequestDTO) {
+            List<FieldError> errors = ex.getBindingResult().getFieldErrors();
+            if (!errors.isEmpty()) {
+                FieldError fieldError = errors.get(0);
+                String field = fieldError.getField();
+                switch (field) {
+                    case "targetId" -> errorMessage = "Invalid product data: Missing or invalid target id.";
+                    case "value" -> errorMessage = "Invalid product data: value must be at least 0.0.";
+                }
+            }
         }
 
-        responseExceptionUtil.writeErrorResponse(response, status, ex.getClass().getSimpleName(), errorMessage, request.getRequestURI());
+        responseExceptionUtil.writeErrorResponse(
+                response,
+                status,
+                ex.getClass().getSimpleName(),
+                errorMessage,
+                request.getRequestURI()
+        );
     }
 }
